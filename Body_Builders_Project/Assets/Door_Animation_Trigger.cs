1a46ad24f4c5c7836a4916664212cd629a3e201b
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-05-14 19:21:42 +0800

    Intial Commit

---
+++ Body_Builders_Project/Assets/Door_Animation_Trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Animation_Trigger : MonoBehaviour
+{
+    public GameObject Door;
+    public Door_Animation_Catcher doorAnimCatch;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject)
+        {
+            Debug.Log("collided with button");
+           // Animation = Door.GetComponent();
+        }
+    }
+}
---
+++ Body_Builders_Project/Assets/Scripts/Air_Lift.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Air_Lift : MonoBehaviour
+{   public float gravity = -9.81f;
+    public float force = 10.81f;
+
+    void Start()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var coll = ps.collision;
+        coll.enabled = true;
+        coll.bounce = 0.5f;
+    }
+}
