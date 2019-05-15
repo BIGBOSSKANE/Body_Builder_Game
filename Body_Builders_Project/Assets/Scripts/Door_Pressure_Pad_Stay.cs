dcde31c1d0f15638c817d31ff4e1aec03bb8c5b0
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-05-15 13:45:55 +0800

    Made More pressure Pads
    I have made two more Pressure Pads
    
    - Bool pressure pad: has an OnTriggerStay function that opens door on staying on the trigger. with an OnExitTrigger that closes the door on leaving the trigger.
    
    Timed Pressure Pad: has an OnEnterTrigger that opens the door and starts a timer, when the selected amount of time has been reached the door closes.
    
    Issues/Bugs: the animation of the doors opening loops in the timed pressure Pad.
    
    Note: I have checked the animations and unchecked their loop.

---
+++ Body_Builders_Project/Assets/Scripts/Door_Pressure_Pad_Stay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Pressure_Pad_Stay : MonoBehaviour
+{
+    public GameObject door;
+    public string animationName = "Opening";
+    public string animationName2 = "Closing";
+
+
+    // Update is called once 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject)
+        {
+            {
+                Debug.Log("collided with button");
+                Animator a = door.GetComponent<Animator>();
+                if (a != null)
+                {
+                    a.Play(animationName);
+                }
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject)
+        {
+            {
+                Debug.Log("collided with button");
+                Animator a = door.GetComponent<Animator>();
+                if (a != null)
+                {
+                    a.Play(animationName2);
+                }
+            }
+        }
+    }
+}
---
+++ Body_Builders_Project/Assets/Scripts/Door_Pressure_Pad_Timed.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Pressure_Pad_Timed : MonoBehaviour
+{
+    public GameObject door;
+    public string animationName = "Opening";
+    public string animationName2 = "Closing";
+    public float timer = 5f;
+
+    // Update is called once 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject)
+        {
+            {
+                Debug.Log("collided with button");
+                Animator a = door.GetComponent<Animator>();
+                if (a != null)
+                {
+                    a.Play(animationName);
+                    StartCoroutine("ClosingDoor");
+                    Debug.Log("I'm looping");
+                }
+            }
+        }
+    }
+    private IEnumerator ClosingDoor()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timer); // wait a selected amount of time
+            Debug.Log("Time's up, Door down");
+                Animator a = door.GetComponent<Animator>();
+                if (a != null)
+                {
+                a.Play(animationName2);
+                }
+            }
+        }
+    }
