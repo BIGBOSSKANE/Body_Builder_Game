884282a4ae3829612396fe2d601f746971c0291a
Brad <brad.power@gmail.com>
2019-05-16 16:58:22 +0800

    Imported V-Es Animations. Delete your library before using.

---
+++ Body_Builders_Project/Assets/Scripts/Resetter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Resetter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
