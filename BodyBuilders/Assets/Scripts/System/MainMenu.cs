228707e16ed17c4754da4e14e7c997571c7ecd1a
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-08-16 00:12:26 +0800

    Main Menu and pause feature

---
+++ BodyBuilders/Assets/Scripts/System/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿/*Author Kane Girvan
+ * 15/8/19
+ * Script to navigate the main menu and level selector
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // this is allows you to use certain classes from SceneManagement (it's used to navigate different scenes in this game project(that is set in the build settings)
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Main; // put a panel for the Main Menu here
+    public GameObject levelSelect; // put a panel for the Level Selection Menu here
+    bool islvlslct = false; // a private(cannot be changed outside the script) on off switch called isLvlSlct
+
+    void Update()
+    {
+        if (islvlslct) // and the bool islvlslct is set to true
+        {
+            Main.SetActive(false); // disable the Main Menu panel
+            levelSelect.SetActive(true); // activate the Level Select menu panel
+        }
+
+        else // else ( the opposite of the previous if statement), if the bool islvlslct is set to false
+        {
+            Main.SetActive(true); // activate the Main Menu panel
+            levelSelect.SetActive(false); // disable the Level Select menu panel
+        }
+    }
+
+    // used for the New Game Button
+    public void NewGameButton() // a selectabel(public) function called NewGameButton
+    {
+        Debug.Log("Start game"); // in the console type Start game
+        SceneManager.LoadScene(1); // in the SceneManager (build settings) load the scene numbered 1 in the index
+    }
+    // used for the Level Selection button
+    public void LevelSelectionButton() // a selectable(public) function called LevelSelectionButton
+    {
+        islvlslct = true; // set the bool islvlslct is set to true
+    }
+    // used for the Return Button
+    public void ReturnButton() // a selectable(public) function called ReturnButton
+    {
+        islvlslct = false; // set the bool islvlslct is set to false
+    }
+    // used for the buttons to select levels
+    public void LevelButton(int level) // a selectable(public) function called LevelButton, when setting up the button write the number of that scenes index
+    {
+        SceneManager.LoadScene(level); // in the SceneManager (build settings) load the scene number writen in the onclick () in the index
+    } 
+
+public void QuitGameButton() // a selectable(public) function called QuitGameButton
+    {
+        Debug.Log("Quited the game"); // in the console type Quited the game
+        Application.Quit(); // close the game
+    }
+}
---
+++ BodyBuilders/Assets/Scripts/System/Pause.cs
@@ -0,0 +1,61 @@
+﻿/*Author Kane Girvan
+ * 15/08/19
+ * Pause system
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // this is allows you to use certain classes from SceneManagement (it's used to navigate different scenes in this game project(that is set in the build settings)
+
+public class Pause : MonoBehaviour
+{ // fields  V  can be set to private or public to be seen in the inspector, on the script. (you don't need to write private, you can even have it as "bool isPaused = true;" and it will automatically be private)
+    private bool isPaused = false; // a private(cannot be changed outside the script) on off switch called isPaused
+    public GameObject pausePanel; // put a panel for the pause menu here
+    public GameObject exitPanel; // put a panel for the exit clarification menu here
+
+    void Update() //on everyframe..
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) // if you press the escape key..
+        {
+            if (isPaused) // and the bool isPaused is set to true
+            {
+                Resume(); // start the function Resume
+            }
+
+            else // else ( the opposite of the previous if statement), if the bool isPaused is set to false
+            {
+                Paused(); // start the function Paused
+            }
+        }
+    }
+    // just like setting up the field up top, you can have to write 'private' before 'void Paused() instead "private void Paused ()"
+    void Paused () // a hidden(private) function called pause
+    { 
+        pausePanel.SetActive(true); // activate the Pause menu
+        Time.timeScale = 0; // set time to stop (0)
+        isPaused = true; // set the bool isPaused is set to true
+    }
+
+    public void Resume () // a selectable(public) function called resume
+    {// this function is set to a button and when the player presses the 'Esc' key
+        pausePanel.SetActive(false); // disable the Pause menu
+        Time.timeScale = 1; // set time to normal (1)
+        isPaused = false; // set the bool isPaused is set to false
+        exitPanel.SetActive(false); // disable the Exit menu
+    }
+
+    public void MainMenu() // a selectable(public) function called MainMenu
+    {
+        exitPanel.SetActive(true); // activate the Exit menu
+    }
+
+    public void CancelExit() // a selectable(public) function called CancelExit
+    {
+        exitPanel.SetActive(false); // disable the Exit menu
+    }
+
+    public void Exit() // a selectable(public) function called Exit
+    {
+        SceneManager.LoadScene(0); // in the SceneManager (build settings) load the scene numbered 0 in the index
+    } // you can have it use the name of the scene instaed of the number it's indexed as "SceneManger.LoadScene(MainMenu);" if it you have put that scene in the build setting
+}
