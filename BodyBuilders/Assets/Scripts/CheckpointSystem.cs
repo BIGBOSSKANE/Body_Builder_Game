1fa9e781472d892365a9a99c4b6cefe44bab5ff2
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-08-23 22:00:43 +0800

    Added the assets for a respawn station

---
+++ BodyBuilders/Assets/Scripts/CheckpointSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointSystem : MonoBehaviour
+{
+    private GameObject _currentSpawnPoint;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Respawn")
+        {
+            _currentSpawnPoint = col.gameObject;
+        }
+        else if(col.tag == "Death")
+        {
+            Respawn();
+        }
+    }
+
+    public void Respawn()
+    {
+        if (_currentSpawnPoint != null)
+        {
+            transform.position = _currentSpawnPoint.transform.position;
+        }
+    }
+}
