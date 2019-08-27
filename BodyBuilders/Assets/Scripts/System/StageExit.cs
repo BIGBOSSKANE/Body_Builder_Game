121f0feaea9f249cfd92f2c2f40a92055b9d131f
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-08-27 19:25:57 +0800

    World save implimented, need a test
    I have made a world save, that remembers how many levels the player has gotten to, we don't have levels yet, but this code might work, tweeked the exit stage prefab,  and made the pause canvas a prefab.

---
+++ BodyBuilders/Assets/Scripts/System/StageExit.cs
@@ -0,0 +1,30 @@
+﻿/*Author Kane
+ * 15/05/19
+ * Loads the next level on trigger
+ */
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; // import SceneMangement namespaces
+
+public class StageExit : MonoBehaviour
+{
+    private int SceneNumber;
+
+    private void Start()
+    {
+        SceneNumber = SceneManager.GetActiveScene().buildIndex + 1; // The SceneName is equaled to the currently active scene number + 1
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {        
+        if (other.gameObject.tag == "Player") // If an other object that has the tag Player collides with this objects 2D Collider
+        {
+            Debug.Log("Win! load next level");
+            if (PlayerPrefs.GetInt("levelReached") < SceneNumber)
+            {
+                PlayerPrefs.SetInt("levelReached", (SceneNumber));
+            }
+            SceneManager.LoadScene(SceneNumber); //In scene manager, Load scene number equalled to SceneName
+        }
+    }
+}
