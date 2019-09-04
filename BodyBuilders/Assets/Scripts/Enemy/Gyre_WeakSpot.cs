a62b8eb78f531de7cba213adb6d52eab9bced989
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-09-04 21:22:06 +0800

    reworking Gyre 1.5
    Gyre will no longer go through walls.
    Player will bounce when jumped on, *but will die when doing so.

--- BodyBuilders/Assets/Scripts/Enemy/Gyre_WeakSpot.cs
+++ BodyBuilders/Assets/Scripts/Enemy/Gyre_WeakSpot.cs
@@ -5,19 +5,40 @@ using UnityEngine;
 public class Gyre_WeakSpot : MonoBehaviour
 {
     public GameObject Station;
+    public float normalJumpForce; // standard bounce force applied
+    Rigidbody2D colRb; // the collider of this object
+    GameObject player; // the player gameObject
+    public float jumpForceMultiplier = 1.2f; // increase the amount of bounce force applied if the player is pressing up
+    float jumpForce; // the actual force applied (one of the ones above)
+
+    void Start()
+    {
+        player = GameObject.Find("Player").gameObject;
+    }
+
+    void Update()
+    {
+        jumpForce = normalJumpForce;
+    }
 
     public void OnTriggerEnter2D(Collider2D col)
     {
+     
+        if (col.gameObject.CompareTag("Station"))
         {
-            if (col.gameObject.CompareTag("Station"))
-            {
-                Station = col.gameObject;
-                Debug.Log("collided");
-            }
+            Station = col.gameObject;
         }
-        if (col.gameObject.CompareTag("Player"))
+     
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
         {
-            Debug.Log("Gyre died");
+            Debug.Log("bounce");
+            Rigidbody2D rb2d = col.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 velocity = rb2d.velocity;
+            velocity.y = jumpForce;
+            rb2d.velocity = velocity;
             if (Station != null)
             {
                 Station.GetComponent<Gyre_StationScript>().StartSpawn();
---
+++ BodyBuilders/Assets/Scripts/Enemy/NewGyre.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGyre : MonoBehaviour
+{
+    public float velocity = 2.5f;
+
+    public Transform sightStart;
+    public Transform sightEnd;
+
+    public LayerMask DetectWhat;
+
+    public bool colliding;
+
+    Rigidbody2D rb2d;
+    playerScript playerScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GameObject.Find("Player").gameObject.GetComponent<playerScript>(); // we can swap this out for the scene manager once it has been added
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity = new Vector2(velocity, rb2d.velocity.y);
+
+        colliding = Physics2D.Linecast(sightStart.position, sightEnd.position, DetectWhat);
+
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+            velocity *= -1;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            playerScript.Respawn(0.2f);
+        }
+    }
+}
