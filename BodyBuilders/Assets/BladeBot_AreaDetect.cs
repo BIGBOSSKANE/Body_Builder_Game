e9c76e0db7d67becad8fc2d4122a23fdb0d87a66
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-09-07 19:40:49 +0800

    BladeBots update 0.8
    Started working on blade bots.

---
+++ BodyBuilders/Assets/BladeBot_AreaDetect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeBot_AreaDetect : MonoBehaviour
+{
+    public GameObject bB;
+    NewBladeBot nBB;
+
+    void Start()
+    {
+        nBB = bB.GetComponent<NewBladeBot>();
+    }
+
+        void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Player entered area");
+            nBB.chasePlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Player entered area");
+            nBB.chasePlayer = false;
+        }
+    }
+}
---
+++ BodyBuilders/Assets/Scripts/Enemy/NewBladeBot.cs
@@ -0,0 +1,39 @@
+﻿//https://www.youtube.com/watch?v=BePP-jtrs9U
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBladeBot : MonoBehaviour
+{
+    public float speed = 5.0f; 
+    public GameObject BBArea;
+    public bool chasePlayer = false;
+    Transform startpos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      // set initial starting point  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chasePlayer)
+        {
+            // follow player
+        }
+        else
+        {
+            // return to start position
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collider2D col)
+    {
+      // when collided with player
+      // respawn player
+    }
+}
