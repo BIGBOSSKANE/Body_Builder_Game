28f49087fdd7495c592805ca56ccfe7ad46d1968
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-09-07 11:11:14 +0800

    Gyre 1.8
    Fixed the issues with the Gyre

--- BodyBuilders/Assets/Scripts/Enemy/Gyre_WeakSpot.cs
+++ BodyBuilders/Assets/Scripts/Enemy/Gyre_WeakSpot.cs
@@ -4,40 +4,25 @@ using UnityEngine;
 
 public class Gyre_WeakSpot : MonoBehaviour
 {
+    [Tooltip("Do not assign, unless Gyre is not on the same platform as the Station.")]
     public GameObject Station;
-    public float normalJumpForce; // standard bounce force applied
     Rigidbody2D colRb; // the collider of this object
     GameObject player; // the player gameObject
-    public float jumpForceMultiplier = 1.2f; // increase the amount of bounce force applied if the player is pressing up
-    float jumpForce; // the actual force applied (one of the ones above)
-
-    void Start()
-    {
-        player = GameObject.Find("Player").gameObject;
-    }
-
-    void Update()
-    {
-        jumpForce = normalJumpForce;
-    }
+    [Tooltip("The strength of the bounce off the gyre.")]
+    public float bounceStrength; // standard jump force applied
 
     public void OnTriggerEnter2D(Collider2D col)
     {
-     
         if (col.gameObject.CompareTag("Station"))
         {
             Station = col.gameObject;
         }
-     
-    }
-    void OnCollisionEnter2D(Collision2D col)
-    {
+
         if (col.gameObject.tag == "Player")
         {
-            Debug.Log("bounce");
             Rigidbody2D rb2d = col.gameObject.GetComponent<Rigidbody2D>();
             Vector2 velocity = rb2d.velocity;
-            velocity.y = jumpForce;
+            velocity.y = bounceStrength;
             rb2d.velocity = velocity;
             if (Station != null)
             {
