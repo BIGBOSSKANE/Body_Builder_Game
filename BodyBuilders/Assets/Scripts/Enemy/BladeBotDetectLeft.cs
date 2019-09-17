c2bf1b2dd7a24f3c7ca66121fdff1a10c1b54ef8
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-09-17 12:04:07 +0800

    BladeBot Update

---
+++ BodyBuilders/Assets/Scripts/Enemy/BladeBotDetectLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeBotDetectLeft : MonoBehaviour
+{
+    newBladeBot nBB;
+
+    void Start()
+    {
+        nBB = GameObject.Find("Bladebot").gameObject.GetComponent<newBladeBot>(); // we can swap this out for the scene manager once it has been added
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            nBB.ChargeL();
+        }
+    }
+}
