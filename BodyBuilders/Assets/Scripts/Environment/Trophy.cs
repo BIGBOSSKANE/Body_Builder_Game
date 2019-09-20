201221807fa8568f3f5b1e4d660222c46c784716
BIGBOSSKANE <girvan-kane@hotmail.com>
2019-09-21 02:37:28 +0800

    Level 5 -Needs Test-

---
+++ BodyBuilders/Assets/Scripts/Environment/Trophy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trophy : MonoBehaviour
+{
+    public AudioSource aS;
+
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            aS.Play();
+            Destroy(gameObject);
+        }
+    }
+}
