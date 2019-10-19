c6697f6af60e2db9c6de2a5f1d2f4f441b208c07
Daniel Creusot <djcreusot@gmail.com>
2019-10-19 18:44:54 +0800

    Falling Train

---
+++ BodyBuilders/Assets/Scripts/LevelSpecific/Level1/fallOnCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallOnCollision : MonoBehaviour
+{
+    public bool falling;
+    public Vector2 fallPoint;
+    public float fallAcceleration;
+    float fallSpeed;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            falling = true;
+            Destroy(GetComponent<BoxCollider2D>());
+        }
+    }
+
+    void Update()
+    {
+        if(falling)
+        {
+            fallSpeed += fallAcceleration * Time.deltaTime;
+            transform.position -= new Vector3(0 , fallSpeed * Time.deltaTime , 0);
+            if(transform.position.y <= fallPoint.y)
+            {
+                transform.position = fallPoint;
+                falling = false;
+            }
+        }
+    }
+}
