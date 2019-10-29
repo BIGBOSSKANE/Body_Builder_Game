412416a348a84be01f8258ae1512f0f2b7da9aeb
Daniel Creusot <djcreusot@gmail.com>
2019-10-29 11:33:57 +0800

    Sawblade begging position setters and activate on enter

--- BodyBuilders/Assets/Scripts/Enemy/Sawblades/sawbladeTranslate.cs
+++ BodyBuilders/Assets/Scripts/Enemy/Sawblades/sawbladeTranslate.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sawbladeTranslate : MonoBehaviour
+public class sawbladeTranslate : activate
 {
-    public bool activated = true;
     public float moveTime = 4f;
-    public float currentMoveTime = 0f;
+    public float moveTimer = 0f;
     public float bladeDistance = 5f;
     public float spinSpeed = 200f;
     public bool smoothMove = true;
-    float moveTimer;
     bool forwards = true;
     public float movementLength;
     float previousMovementLength;
@@ -21,20 +19,14 @@ public class sawbladeTranslate : MonoBehaviour
 
     void Start()
     {
-        if(currentMoveTime <0)
-        {
-            forwards = false;
-            currentMoveTime = Mathf.Clamp(Mathf.Abs(currentMoveTime) , 0 , 1);
-        }
         groove.localScale = new Vector3(movementLength * transform.localScale.x , groove.localScale.y , groove.localScale.z);
         float grooveLength = (groove.localScale.x / 2) - (movingSaw.GetComponent<SpriteRenderer>().bounds.extents.x);
         startPos = new Vector2(-grooveLength, -0.44f);
         endPos= new Vector2(grooveLength, -0.44f);
-    }
 
-    void Update()
-    {
-        moveTimer += Time.deltaTime / moveTime;
+        if(moveTimer < 0) forwards= false;
+        moveTimer = Mathf.Clamp(Mathf.Abs(moveTimer) / moveTime , 0 , 1);
+        
         if(forwards)
         {
             if(smoothMove) movingSaw.localPosition = new Vector2(Mathf.SmoothStep(startPos.x , endPos.x , moveTimer) , Mathf.SmoothStep(startPos.y , endPos.y , moveTimer));
@@ -45,11 +37,29 @@ public class sawbladeTranslate : MonoBehaviour
             if(smoothMove) movingSaw.localPosition = new Vector2(Mathf.SmoothStep(endPos.x , startPos.x , moveTimer) , Mathf.SmoothStep(endPos.y , startPos.y , moveTimer));
             else movingSaw.localPosition = Vector2.Lerp(endPos , startPos, moveTimer);
         }
+    }
 
-        if(moveTimer >= 1)
+    void Update()
+    {
+        if(activated)
         {
-            forwards = !forwards;
-            moveTimer = 0;
+            moveTimer += Time.deltaTime / moveTime;
+            if(forwards)
+            {
+                if(smoothMove) movingSaw.localPosition = new Vector2(Mathf.SmoothStep(startPos.x , endPos.x , moveTimer) , Mathf.SmoothStep(startPos.y , endPos.y , moveTimer));
+                else movingSaw.localPosition = Vector2.Lerp(startPos , endPos, moveTimer);
+            }
+            else
+            {
+                if(smoothMove) movingSaw.localPosition = new Vector2(Mathf.SmoothStep(endPos.x , startPos.x , moveTimer) , Mathf.SmoothStep(endPos.y , startPos.y , moveTimer));
+                else movingSaw.localPosition = Vector2.Lerp(endPos , startPos, moveTimer);
+            }
+
+            if(moveTimer >= 1)
+            {
+                forwards = !forwards;
+                moveTimer = 0;
+            }
         }
     }
 
---
+++ BodyBuilders/Assets/Scripts/Environment/Activators/activateOnEnter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateOnEnter : MonoBehaviour
+{
+    [Tooltip("Overcharge activatable dependents")] public bool overcharge = false;
+    [Tooltip("What game objects does this activate, increase the size to add more")] public GameObject [] activates;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            foreach (GameObject activateable in activates)
+            {
+                if(activateable != null)
+                {
+                    activate activateScript = activateable.GetComponent<activate>();
+                    activateScript.Activate(true);
+                    if(overcharge)
+                    {
+                        activateScript.Overcharge(overcharge); // if the power station is overcharged, overcharge the activatable dependent object
+                    }
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
