9492a34297b52b682f2602ff14a3cc7e3df6aa15
Daniel Creusot <djcreusot@gmail.com>
2019-11-16 16:35:45 +0800

    Prefab changes and laser integration

--- BodyBuilders/Assets/Scripts/Enemy/edgelordPlayerCue.cs
+++ BodyBuilders/Assets/Scripts/Enemy/edgelordPlayerCue.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class edgelordPlayerCue : MonoBehaviour
 {
     public GameObject edgelord;
+    public GameObject destroyPrevious;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
         {
+            if(destroyPrevious != null) Destroy(destroyPrevious);
+            edgelord.SetActive(true);
             edgelord.GetComponent<edgelord>().waitingForPlayer = false;
             edgelord.SetActive(true);
             Destroy(gameObject);
