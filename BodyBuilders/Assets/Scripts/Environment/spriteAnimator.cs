370b1b7496a82673d8c5a62f5a0ef621a05cbcba
Daniel Creusot <djcreusot@gmail.com>
2019-11-17 08:10:55 +0800

    Music in those menus

--- BodyBuilders/Assets/Scripts/Environment/spriteAnimator.cs
+++ BodyBuilders/Assets/Scripts/Environment/spriteAnimator.cs
@@ -13,17 +13,20 @@ public class spriteAnimator : MonoBehaviour
     [HideInInspector] public bool reverse = false; // called by the conveyor belt
     [Tooltip("Framerate of animation (overriden by the converyorBelt script)")] [Range (0 , 200)] public float framesPerSecond = 100; // speed through which frames cycle
     public List<Sprite> animationFrames = new List<Sprite>();
+    public bool randomStart = true;
+    int randomStartTime;
      
     void Start()
     {
         frameCount = animationFrames.Count;
+        if(randomStart) randomStartTime = Random.Range(0 , frameCount);
     }
 
     void Update()
     {
         if(animated)
         {
-            animationTime = (Time.time * framesPerSecond) % frameCount; // get the animation time by finding the remainder of the real time to the frame count
+            animationTime = (Time.time * framesPerSecond + randomStartTime) % frameCount; // get the animation time by finding the remainder of the real time to the frame count
             currentFrame = (int)(animationTime); // cycle through frames over time
             if(reverse) currentFrame = frameCount - (currentFrame + 1); // reverse the order
             if(lastFrame != currentFrame) render.sprite = animationFrames[currentFrame]; // if the next frame to allocate isn't the same as the last one, play it
